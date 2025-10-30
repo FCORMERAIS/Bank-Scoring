1688649e3ed359847b9b27fc6c5a5c9f5919f372
FlAvril <92847885+FCORMERAIS@users.noreply.github.com>
2025-10-30 16:39:47 +0100

    changement arboressance

---
+++ app/main.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__, template_folder='./templates')
+
+@app.route('/')
+def home():
+    return "Bienvenue dans mon application Flask ! 🚀"
+
+@app.route('/hello')
+def hello():
+    return 'Bonjour depuis Flask ! 👋'
+    
+@app.route('/bonjour/<prenom>')
+def greet(prenom):
+    return f"Bonjour, {prenom.capitalize()} ! 😄"
+
+@app.route('/articles', methods=['GET'])
+def list_articles():
+    return {'articles': ['Article 1', 'Article 2', 'Article 3']}
+
+@app.route('/articles', methods=['POST'])
+def add_article():
+    data = request.json
+    article = data.get('article')
+    # Ajouter l'article à une base de données
+    return {'message': f"Article '{article}' ajouté avec succès! ✅"}, 201
+
+@app.route('/page')
+def page_html():
+    try:
+        return render_template('page.html')
+    except Exception as e:
+        return f"Erreur: {str(e)}", 500
